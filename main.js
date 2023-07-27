e523a30c4383082e9ec97603da5a6bad79ae8a3b
SpaceChart <118946710+SpaceChart@users.noreply.github.com>
2023-07-27 12:16:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(650, 500);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide(); 
+    tint_color = "";
+}
+function draw() {
+    image(video, 0, 0, 650, 500);
+    tint(tint_color);
+}
+function take_snapshot() {
+    save("filterphoto.png")
+}
+function filter_tint()
+{
+    tint_color = document.getElementById("color").value;
+}
